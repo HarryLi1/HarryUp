37ca8b31afade6ceec362d472feb5ddf0304d4de
HarryLi1 <lihao_harry@msn.com>
2016-04-22 18:41:18 +0800

    Add Algorithm.Core

---
+++ Projects/Algorithm/Algorithm.Core/Entry/BTree.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmCore.Entry
+{
+    public class BTree
+    {
+        public int Value;
+        public BTree Left;
+        public BTree Right;
+    }
+}
