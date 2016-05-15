926c63130e4c558d91bc3ff6c6737bf0a08260c9
Harry Li <lihao_harry@msn.com>
2016-05-15 21:23:42 +0800

    添加建造者模式.

---
+++ Projects/DesignModel/BuilderModel/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BuilderModel
+{
+    static class Program
+    {
+        static void Main()
+        {
+            Director.GetABenzModel().Run();
+            Director.GetBBenzModel().Run();
+
+            Director.GetCBMWModel().Run();
+            Director.GetDBMWModel().Run();
+
+        }
+    }
+}
